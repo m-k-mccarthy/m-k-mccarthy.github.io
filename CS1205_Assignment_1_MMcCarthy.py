0a312ce3af0c2292d304158f5d56770bf824c277
m-k-mccarthy <125106449@umail.ucc.ie>
2025-11-11 14:42:40 +0000

    Add files via upload

--- CS1205_Assignment_1_MMcCarthy.py
+++ CS1205_Assignment_1_MMcCarthy.py
@@ -7,8 +7,6 @@ student number: 125106449
 """
 """
 steps left to do:
-    change inputs to in-code values??
-    change classif to add invalidated values
     make sure comments are right
 """
 
@@ -25,15 +23,15 @@ def BMI (height,weight):
 
 
 # defining a function to convert imperial height to metric height
-def con_h (a,b):
+def con_h (foot,inch):
    
     # converts feet and inches to meters
-    if a >=0 and b >=0:
-        foot = a / 3.281
-        inch = b / 39.37
+    if foot >=0 and inch >=0:
+        converted_foot = foot / 3.281
+        converted_inch = inch / 39.37
         
         # returns total height
-        return foot + inch
+        return converted_foot + converted_inch
     
     # setting a fault control
     else:
@@ -41,15 +39,15 @@ def con_h (a,b):
    
  
 # defining a function to convert imperial weight to metric weight
-def con_w (x, y):
+def con_w (stone, pound):
     
     # converts stones and pounds to kilograms
-    if x >=0 and y >=0:
-        stone = x * 6.35
-        pound = y / 2.205
+    if stone >=0 and pound >=0:
+        converted_stone = stone * 6.35
+        converted_pound = pound / 2.205
         
         # returns total weight
-        return stone + pound
+        return converted_stone + converted_pound
     
     # setting a fault control
     else:
@@ -58,20 +56,20 @@ def con_w (x, y):
  
 # defining a function to find the BMI classification based on 
 # earlier BMI value
-def classif (m):
-    if m <0:
+def classif (bmi):
+    if bmi <=0:
         outcome = 'N/A'
-    elif m <18.5:
+    elif bmi <18.50:
         outcome = 'underweight'
-    elif m <25.0:
+    elif bmi <25.00:
         outcome = 'healthy weight'
-    elif m <30.0:
+    elif bmi <30.00:
         outcome = 'overweight'
-    elif m <35.0:
+    elif bmi <35.00:
         outcome = 'obesity I'
-    elif m <40.0:
+    elif bmi <40.00:
         outcome  = 'obesity II'
-    elif m >= 40.0:
+    elif bmi >= 40.00:
         outcome = 'obesity III'
     
     return outcome
@@ -119,7 +117,7 @@ print('Your weight in kgs is', con_w(10, 10))
 # QUESTION 3
 
 # calling functions to determine full BMI classification and setting output
-outcome = classif(23.5)
+outcome = classif(25)
 
 # setting output message for this question and the next
 msg = 'Your BMI weight classification is {}'
@@ -132,7 +130,7 @@ print(msg.format(outcome))
 # QUESTION 4
 
 # calling functions to convert all values and determine full BMI classification
-outcome = patient_full_classif(imperial_feet,imperial_inches,imperial_stones,imperial_pounds)
+outcome = patient_full_classif(5,11,10,10)
 
 # printing output message
 print(msg.format(outcome))    
